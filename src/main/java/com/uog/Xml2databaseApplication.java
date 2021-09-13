be6206c087c277ed75115dd187b0c16e6354c7d3
Nauman Riaz Chaudhry <nauman.riaz@gmail.com>
2021-09-13 14:40:08 +0500

    Initial update

---
+++ src/main/java/com/uog/Xml2databaseApplication.java
@@ -0,0 +1,13 @@
+package com.uog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Xml2databaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Xml2databaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uog/Xml2databaseApplicationTests.java
@@ -0,0 +1,17 @@
+package com.uog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Xml2databaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
